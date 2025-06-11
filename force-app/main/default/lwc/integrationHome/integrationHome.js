133a74ae8f0f2eab14895c7894df80dc95f1e7ef
Sanket-walanj <code.sanky@gmail.com>
2025-06-11 11:39:58 +0530

    lwc portal

--- force-app/main/default/lwc/integrationHome/integrationHome.js
+++ force-app/main/default/lwc/integrationHome/integrationHome.js
@@ -1,3 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from "lwc";
+import getAllAccounts from "@salesforce/apex/IntegrationHome.getAllAccounts";
 
-export default class IntegrationHome extends LightningElement {}
+export default class IntegrationHome extends LightningElement {
+  accounts;
+  error;
+
+  @wire(getAllAccounts)
+  wiredAccounts({ error, data }) {
+    if (data) {
+      this.accounts = data;
+      console.log("Accounts data:", this.accounts);
+      // Reset error if data is successfully retrieved
+      this.error = undefined;
+    } else if (error) {
+      this.error = error;
+      console.log("Accounts error:", this.error);
+      this.accounts = undefined;
+    }
+  }
+}
---
+++ force-app/main/default/lwc/lwcPortal/lwcPortal.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class LwcPortal extends LightningElement {}
