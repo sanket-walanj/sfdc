cf5110713fe08042977981d29a0e5f610e64081f
Sanket-walanj <code.sanky@gmail.com>
2025-06-12 12:39:31 +0530

    LWC Portal Tabs

--- force-app/main/default/lwc/lwcPortal/lwcPortal.js
+++ force-app/main/default/lwc/lwcPortal/lwcPortal.js
@@ -1,3 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from "lwc";
 
-export default class LwcPortal extends LightningElement {}
+export default class MyTabs extends LightningElement {
+  @track activeTab = "tab1";
+  @track isTab1 = true;
+  @track isTab2 = false;
+  @track isTab3 = false;
+
+  handleTabClick(event) {
+    this.activeTab = event.currentTarget.dataset.tab;
+    console.log("this.activeTab = ", this.activeTab);
+
+    if (this.activeTab === "tab1") {
+      this.isTab1 = true;
+      this.isTab2 = false;
+      this.isTab3 = false;
+    } else if (this.activeTab === "tab2") {
+      this.isTab1 = false;
+      this.isTab2 = true;
+      this.isTab3 = false;
+    } else {
+      this.isTab1 = false;
+      this.isTab2 = false;
+      this.isTab3 = true;
+    }
+  }
+}
--- force-app/main/default/triggers/LeadTrigger.trigger
+++ force-app/main/default/triggers/LeadTrigger.trigger
@@ -1,12 +1,10 @@
-trigger LeadTrigger on Lead (before update, after insert){
-    
-    switch on Trigger.operationType{
-        when BEFORE_UPDATE{
-            LeadTriggerHandler.beforeUpdateHandler(Trigger.new);
-        }
-        
-        when AFTER_INSERT{
-            LeadTriggerHandler.afterInsertHandler(Trigger.new);
-        }
+trigger LeadTrigger on Lead(before update, after insert) {
+  switch on Trigger.operationType {
+    when BEFORE_UPDATE {
+      LeadTriggerHandler.beforeUpdateHandler(Trigger.new);
     }
+    when AFTER_INSERT {
+      LeadTriggerHandler.afterInsertHandler(Trigger.new);
+    }
+  }
 }
